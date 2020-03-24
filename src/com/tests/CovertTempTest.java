44f449cb017ab3dc0e755596de64572fd0535ed6
gouravganguly <gourav1ganguly@gmail.com>
2020-03-24 02:22:12 +0000

    Add code and tests

---
+++ src/com/tests/CovertTempTest.java
@@ -0,0 +1,15 @@
+package com.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CovertTempTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+	
+
+}
